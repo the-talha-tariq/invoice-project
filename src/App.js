6a3d3b5b2ad69aca862c388c17d9b93a42c6832c
Talha Tariq <tariqtalha936@gmail.com>
2023-08-07 11:46:19 +0500

    Start Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const handlePrint=()=>{
+    window.print();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+
+    <main className='m-5 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow'>
+      {/* header */}
+      <header className="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between">
+        <div>
+          <h1 className='font-bold uppercase tracking-wide text-4xl mb-3'>Invoice</h1>
+        </div>
+          <div>
+            <ul className="flex items-center justify-between flex-wrap">
+            <li><button className='btn btn-print' onClick={handlePrint}>Print</button></li>
+            <li><button className='btn btn-download'>Download</button></li>
+            <li><button className='btn btn-send'>Send</button></li>
+            </ul>
+          </div>
       </header>
-    </div>
-  );
+
+      {/* Your Detail */}
+
+      <section className='flex flex-col items-end justify-end'>
+        <h2 className='text-xl uppercase' >Thomas Shankar</h2>
+        <p>Your Address</p>
+      </section>
+
+      {/* Client Detail */}
+
+      <section className='mt-5'>
+        <h2 className='text-xl uppercase'>Client's Name</h2>
+        <p>Client's Address</p>
+      </section>
+
+      {/* Date */}
+
+      <article className='my-5 flex items-end justify-end'>
+        <ul>
+          <li><span className='font-bold'>Invoice Number:</span></li>
+          <li><span className='font-bold'>Due Date</span></li>
+          <li><span className='font-bold'>Invoice Date</span></li>
+        </ul>
+      </article>
+
+
+      {/* Tables */}
+
+      <div className='my-5'>This is the Table</div>
+      {/* Notes */}
+      <section className='mb-5'>
+        <p>Notes to the Client...</p>
+      </section>
+
+      {/* Footer */}
+      <footer>
+        <ul className='flex flex-wrap items-center justify-center'>
+          <li><span className='font-bold'>Your Name</span></li>
+          <li><span className='font-bold'>Your Email</span></li>
+          <li><span className='font-bold'>Phone Number</span></li>
+          <li><span className='font-bold'>Bank</span></li>
+          <li><span className='font-bold'>Account Holder Name</span></li>
+          <li><span className='font-bold'>Account Number</span></li>
+          <li><span className='font-bold'>Website</span></li>
+          
+        </ul>
+      </footer>
+
+
+    </main>
+      
+    </>
+  ); 
 }
 
 export default App;
